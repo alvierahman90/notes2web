c51b8a302abac6a78d60dc4eb6e3a27016a6b547
Alvie Rahman <alvierahman90@gmail.com>
2021-08-19 15:31:28 +0100

    Add tags, path to search results

--- search.js
+++ search.js
@@ -11,13 +11,28 @@ function callback() {
   results.innerHTML = ''
   fuse.search(searchBar.value).forEach(r => {
     wrapper = document.createElement('div')
+    wrapper.className = "article"
+
+    extra_info = document.createElement('p')
+    extra_info.className = "smallText"
+    extra_info.innerHTML = "tags: "
+    if (r.item.tags == null) {
+      extra_info.innerHTML += "none"
+    } else {
+      extra_info.innerHTML += "[" + r.item.tags.join(', ') + ']'
+    }
+    extra_info.innerHTML += ' path: ' + r.item.path
+
     content = document.createElement('a')
     content.innerHTML = r.item.title
     content.href = r.item.path
+
     wrapper.appendChild(content)
-    wrapper.className = "article"
+    wrapper.appendChild(extra_info)
+
     results.appendChild(wrapper)
   })
 }
 
 searchBar.addEventListener('keyup', callback)
+callback()
